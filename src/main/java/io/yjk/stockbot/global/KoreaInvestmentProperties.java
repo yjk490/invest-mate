b31d77c4fe4aa81aeb51f4a21851caec02bd7345
yjk490 <yjk490@gmail.com>
2025-03-27 00:19:12 +0900

    feat: KoreaInvestmentProperties

---
+++ src/main/java/io/yjk/stockbot/global/KoreaInvestmentProperties.java
@@ -0,0 +1,59 @@
+package io.yjk.stockbot.global;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+public interface KoreaInvestmentProperties {
+    String baseUrl();
+    String appKey();
+    String appSecret();
+
+    @Configuration
+    @PropertySource("classpath:application-dev.properties")
+    class KoreaInvestmentDevProperties implements KoreaInvestmentProperties {
+
+        @Value("${koreaInvestment.baseUrl}") private String baseUrl;
+        @Value("${koreaInvestment.appKey}") private String appKey;
+        @Value("${koreaInvestment.appSecret}") private String appSecret;
+
+        @Override
+        public String baseUrl() {
+            return baseUrl;
+        }
+
+        @Override
+        public String appKey() {
+            return appKey;
+        }
+
+        @Override
+        public String appSecret() {
+            return appSecret;
+        }
+    }
+
+    @Configuration
+    @PropertySource("classpath:application-prod.properties")
+    class KoreaInvestmentProdProperties implements KoreaInvestmentProperties {
+
+        @Value("${koreaInvestment.baseUrl}") private String baseUrl;
+        @Value("${koreaInvestment.appKey}") private String appKey;
+        @Value("${koreaInvestment.appSecret}") private String appSecret;
+
+        @Override
+        public String baseUrl() {
+            return baseUrl;
+        }
+
+        @Override
+        public String appKey() {
+            return appKey;
+        }
+
+        @Override
+        public String appSecret() {
+            return appSecret;
+        }
+    }
+}
