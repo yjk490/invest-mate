bd62fa8936217139a1ee1ce5eef8904f9fb1e0ea
yjk490 <yjk490@gmail.com>
2025-04-02 01:02:59 +0900

    feat: KoreaInvestmentGatewayClient Integration Test

--- src/test/java/io/yjk/investmate/integration/koreainvestment/infra/gateway/KoreaInvestmentGatewayClientTest.java
+++ src/test/java/io/yjk/investmate/integration/koreainvestment/infra/gateway/KoreaInvestmentGatewayClientTest.java
@@ -1,7 +1,24 @@
 package io.yjk.investmate.integration.koreainvestment.infra.gateway;
 
-import static org.junit.jupiter.api.Assertions.*;
+import io.yjk.investmate.koreainvestment.infra.gateway.KoreaInvestmentGatewayClient;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
+@Slf4j
+@SpringBootTest
+@Import(KoreaInvestmentGatewayClient.class)
 class KoreaInvestmentGatewayClientTest {
 
+    @Autowired
+    private KoreaInvestmentGatewayClient koreaInvestmentGatewayClient;
+
+    @Test
+    void loginAndLogout() {
+        Assertions.assertDoesNotThrow(() -> koreaInvestmentGatewayClient.login());
+        Assertions.assertDoesNotThrow(() -> koreaInvestmentGatewayClient.logout());
+    }
 }
